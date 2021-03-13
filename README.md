46b930a5bf978f79468bca02048dd2fecf4c490d
Akshay Lokur <lokur.akshay@gmail.com>
2021-03-13 08:03:31 +0800

    Added a producer demo class with callback to print certain metadata.

---
+++ README.md
@@ -0,0 +1,5 @@
+Expected that Zookeeper and Kafka is running on your local machine:
+
+- cd <your_kafka_binaries_root_dir>
+- zookeeper-server-start.sh config/zookeeper.properties
+- kafka-server-start config/server.properties
---
+++ src/main/java/org/example/ProducerDemoWithCallback.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+        String bootstrapServers = "localhost:9092";
+
+        // Create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // Create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("second_topic", "Hello World!");
+
+        // Send data
+        producer.send(record, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                // Executes every time a record has been successfully sent or an exception is thrown
+                if (e == null) {
+                    // record successfully sent
+                    logger.info("Received new metadata.\n" +
+                            "Topic: " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp()
+                    );
+                } else {
+                    logger.error("Error occurred while producing", e);
+                }
+
+            }
+        });
+
+        // Flush data and close
+        producer.flush();
+        producer.close();
+    }
+}
